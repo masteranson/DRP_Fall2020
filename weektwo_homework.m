7c3cb60e5886116a0448a7ea6c5ad34a03f5620a
Anson Tsang <ytsang3@gatech.edu>
2020-10-13 23:28:07 +0800

    Update weektwo_homework.m
    Made it working

--- weektwo_homework.m
+++ weektwo_homework.m
@@ -1,6 +1,6 @@
 %% Initial Variables
 t0 = 0; %Initial time
-n = 1000; %steps
+n = 199; %steps
 dt  = 1/n; %Timestep
 h_t = 0.001; %timestep half criterion
 
@@ -9,56 +9,71 @@ syms f1(x,y) f2(x,y) f(x,y)
 
 f1(x,y) = x + y; 
 f2(x,y) = x + y + 1; 
-f(x,y) = f1 + f2; %Target System
+f(x,y) = [f1 ; f2]; %Target System
 
 %% Polynomial Homogenization
-syms z
+syms z %homogenizing variable
 
 degree  = polynomialDegree(f);
-f(x,y,z) = (z^degree).*f(x/z,y/z);
+f(x,y,z) = (z^degree(1)).*f(x/z,y/z);
 
-%% Starting System
-syms fs(x,y)
+%% Starting Solution Generation
 
-%Projective Transformation Euclidean Patch
-syms r(x,y,z)
+%Solutions based on roots of unity [e^i2pi/degree; 1]
+%Solutions before homogenization (canonical chart z0 = 1)
+starting_solutions =[exp(1i*2*pi)/(degree(1)); exp(1i*2*pi)/(degree(2)); 1];
 
-r(x,y,z) = (rand + rand*1i)*x +(rand + rand*1i)*y + (rand + rand*1i)*z - 1;
+%Projective Transformation Euclidean Patch (eliminates scaling)
+syms r(x,y,z) l(x,y,z)
+
+% Generic Linear Form
+l(x,y,z) =(rand + rand*1i)*x +(rand + rand*1i)*y + (rand + rand*1i)*z; 
+
+r(x,y,z) = l - 1; %Generic Patch
+starting_solutions = (1/l(1,1,1))*starting_solutions;
+
+%% Homogenized Total Degree Starting System
+syms fs(x,y) 
 
 %fs = gamma*(zn^d1 - z(n-1)^d1;z(n-(n-1))^d1-z(0)^d1)
-fs(x,y,z) = [(x^degree - y^degree); (y^degree- z^degree)];
+fs(x,y,z) =[(x^degree(1) - z^degree(1)); (y^degree(2) - z^degree(2)); r];
 
 %% Homotopy
 syms  h(x,y,t) p(x,y,t)
 
-gamma = 1/r;
-h(x,y,z,t) =  gamma*fs(x,y,z)*t + f(x,y,z)*(1-t); %Homotopy
+f = [f; r]; %Appended Patch to target solution
+gamma = rand + rand*1i; %Gamma Trick
+h(x,y,z,t) =  gamma*fs(x,y,z)*(1-t) + f(x,y,z)*t; %Homotopy
 
-%initial_solution(t) = solve(h,x,y,z); %x(t) solution of h(x,t)
-%starting_point = initial_solution(t0);
+%% Predictor Corrector
 
-dht = diff(h,t);
-j = jacobian(h(x,y,z,t),[x,y,z,t]);
+dht = jacobian(h,t);
+jac = jacobian(h,[x,y,z]);
 
-p(x,y,z,t) = j^(-1)*dht; %Predictor
-j = jacobian(h(x,y,z,t0),[x,y,z,t])^(-1)*h(x,y,z,t0); %Corrector
-
+p(x,y,z,t) = jac^(-1)*dht; %Predictor
+j = jac^(-1)*h; %Newton's Corrector f(x)/f'(x)
 
 %% Predictor Corrector Loop
 %TODO: Convert everything to numerical calculation for speedup
 %TODO: Parallelize Code
 
-xval = double(starting_point); %tracking variable
+xval = double(starting_solutions); %tracking variable
 tval = t0;
 
 for counter = 1:n
     
     xval_temp = xval;
     tval_temp = tval;
-    xval = double(xval + p(xval,tval)*dt); % Predicted Value
-    xval = xval - j(xval,tval)*h(xval,tval);% Corrected Value
+    xval = double(xval + p(xval(1),xval(2),xval(3),tval)*dt); % Predicted Value
+    xval = xval - j(xval(1),xval(2),xval(3),tval+dt);% Corrected Value
     tval = tval+ dt;   
 
+    %TODO: Visualization R^3 [Re,Ir, T]
+    fprintf('Solution step %d: %f %f %f\n', counter, xval(1), xval(2),xval(3));
+    fprintf('Residual: %f\n',norm(h(xval(1),xval(2),xval(3),tval)));
+
+    %Adaptive step size
+    %TODO: Implement step size bigger criterion
     if mod(counter,200) == 0
         fprintf('Iteration %d\n',counter)
         half_dt = dt/2;
@@ -67,7 +82,7 @@ for counter = 1:n
             xval_temp = xval_temp - j(xval_temp,tval)*h(xval_temp,tval);% Corrected Value
             tval_temp = tval+ half_dt;
         end          
-        if abs(xval-xval_temp)/xval > h_t %Comparison
+        if norm(xval-xval_temp)/norm(xval) > h_t %Comparison
             dt = half_dt;
             fprintf('Timestep halved to %f\n',dt);
             xval = xval_temp;
@@ -76,10 +91,11 @@ for counter = 1:n
 end
 
 %% Results
-
-tracked_solutions = length(xval);
-fprintf('Tracked %d solutions:\n',tracked_solutions);
-
-for counter = 1:tracked_solutions
-    fprintf('Solution %d: %f%+fi\n', counter, [real(xval(counter)), imag(xval(counter))]);
-end
+%TODO: Fix it
+% 
+% tracked_solutions = length(xval);
+% fprintf('Tracked %d solutions:\n',tracked_solutions);
+% 
+% for counter = 1:tracked_solutions
+%     fprintf('Solution %d: %f%+fi\n', counter, [real(xval(counter)), imag(xval(counter))]);
+% end
