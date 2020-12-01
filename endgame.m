ebaf5212fa5f201b988692b1c1433aee0cb2857c
Anson Tsang <ytsang3@gatech.edu>
2020-12-01 23:10:37 +0800

    Endgame crude implementation but not working

---
+++ endgame.m
@@ -0,0 +1,20 @@
+function [xval,num_solutions, tracking_values] = endgame(jac_eval,  xval,  tval, tracking_values)
+
+condition_threshold = 10^5;
+num_solutions = length(xval);
+xval_new = xval;
+tracking_values_new = tracking_values;
+
+for counter = 1:length(xval) %Truncating tracking solutions based on predictor condition number
+    condition_number = cond(jac_eval(xval(counter,1),xval(counter,2),xval(counter,3),tval));
+    if condition_number > condition_threshold
+        xval_new(counter,:) = [];
+        num_solutions = num_solutions - 1;
+        tracking_values_new(counter,:,:) = [];      
+        fprintf('Removed Tracking Solution with Condition Number %d\n',condition_number);
+    end
+end
+
+xval = xval_new;
+tracking_values = tracking_values_new;
+end
--- parameter_homotopy.m
+++ parameter_homotopy.m
@@ -1,8 +1,10 @@
-function [predictor, corrector] = parameter_homotopy(f_t, parameters, start_values, target_values, variables, patch)
+function [predictor, corrector, jac, h] = parameter_homotopy(f_t, parameters, start_values, target_values, variables, patch)
+
+syms t
 
 starting_system = subs(f_t, parameters, start_values);
 target_system = subs(f_t, parameters, target_values);
-[predictor, corrector] = homotopy_generator([starting_system; patch], target_system, patch, variables);
+[predictor, corrector, jac, h] = homotopy_generator([starting_system; patch], target_system, patch, variables, t);
 
 end
 
